f9e20c2c3bddf5d61e54f1dd356cbd4808608c9b
mjoerussell <mjoerussell@gmail.com>
2023-08-01 13:20:48 -0500

    Removing the date_calculator factories

--- lib/holidays/load_all_definitions.rb
+++ lib/holidays/load_all_definitions.rb
@@ -1,4 +1,6 @@
 require "holidays/date_calculator/day_of_month"
+require "holidays/date_calculator/lunar_date"
+require "holidays/date_calculator/weekend_modifier"
 
 module Holidays
   #TODO This file should be renamed. It's no longer about definitions, really.
@@ -17,47 +19,26 @@ module Holidays
         #
         # NOTE: These are no longer public! We can do whatever we want here!
         global_methods = {
-          "easter(year)" => gregorian_easter.method(:calculate_easter_for).to_proc,
-          "orthodox_easter(year)" => gregorian_easter.method(:calculate_orthodox_easter_for).to_proc,
-          "orthodox_easter_julian(year)" => julian_easter.method(:calculate_orthodox_easter_for).to_proc,
-          "to_monday_if_sunday(date)" => weekend_modifier.method(:to_monday_if_sunday).to_proc,
-          "to_monday_if_weekend(date)" => weekend_modifier.method(:to_monday_if_weekend).to_proc,
-          "to_weekday_if_boxing_weekend(date)" => weekend_modifier.method(:to_weekday_if_boxing_weekend).to_proc,
-          "to_weekday_if_boxing_weekend_from_year(year)" => weekend_modifier.method(:to_weekday_if_boxing_weekend_from_year).to_proc,
-          "to_weekday_if_weekend(date)" => weekend_modifier.method(:to_weekday_if_weekend).to_proc,
+          "easter(year)" => Holidays::DateCalculator::Easter::Gregorian.method(:calculate_easter_for).to_proc,
+          "orthodox_easter(year)" => Holidays::DateCalculator::Easter::Gregorian.method(:calculate_orthodox_easter_for).to_proc,
+          "orthodox_easter_julian(year)" => Holidays::DateCalculator::Easter::Julien.method(:calculate_orthodox_easter_for).to_proc,
+          "to_monday_if_sunday(date)" => Holidays::DateCalculator.method(:to_monday_if_sunday).to_proc,
+          "to_monday_if_weekend(date)" => Holidays::DateCalculator.method(:to_monday_if_weekend).to_proc,
+          "to_weekday_if_boxing_weekend(date)" => Holidays::DateCalculator.method(:to_weekday_if_boxing_weekend).to_proc,
+          "to_weekday_if_boxing_weekend_from_year(year)" => Holidays::DateCalculator.method(:to_weekday_if_boxing_weekend_from_year).to_proc,
+          "to_weekday_if_weekend(date)" => Holidays::DateCalculator.method(:to_weekday_if_weekend).to_proc,
           "calculate_day_of_month(year, month, day, wday)" => Holidays::DateCalculator.method(:day_of_month).to_proc,
-          "to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday(year)" => weekend_modifier.method(:to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday).to_proc,
-          "to_tuesday_if_sunday_or_monday_if_saturday(date)" => weekend_modifier.method(:to_tuesday_if_sunday_or_monday_if_saturday).to_proc,
-          "lunar_to_solar(year, month, day, region)" => lunar_date.method(:to_solar).to_proc, 
+          "to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday(year)" => Holidays::DateCalculator.method(:to_weekday_if_boxing_weekend_from_year_or_to_tuesday_if_monday).to_proc,
+          "to_tuesday_if_sunday_or_monday_if_saturday(date)" => Holidays::DateCalculator.method(:to_tuesday_if_sunday_or_monday_if_saturday).to_proc,
+          "lunar_to_solar(year, month, day, region)" => Holidays::DateCalculator.method(:to_solar).to_proc, 
         }
 
         Factory::Definition.custom_methods_repository.add(global_methods)
-
-        puts "Loaded custom methods: "
-        puts Factory::Definition.custom_methods_repository
       end
 
       def load_regions
         static_regions_definition = "#{Holidays::configuration.definitions_path}/REGIONS.rb"
         require static_regions_definition
-      end
-
-      private
-
-      def gregorian_easter
-        Factory::DateCalculator::Easter::Gregorian.easter_calculator
-      end
-
-      def julian_easter
-        Factory::DateCalculator::Easter::Julian.easter_calculator
-      end
-
-      def weekend_modifier
-        Factory::DateCalculator.weekend_modifier
-      end
-
-      def lunar_date
-        Factory::DateCalculator.lunar_date
       end
     end
   end
