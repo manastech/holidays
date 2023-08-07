2f0d4b467a1c4c778f1813be5a7ff1e20f7b1eb6
mjoerussell <mjoerussell@gmail.com>
2023-08-07 14:50:07 -0500

    Don't try to overload CustomMethod.new

--- lib/holidays/definition/custom_method.rb
+++ lib/holidays/definition/custom_method.rb
@@ -8,7 +8,7 @@ module Holidays
     attr_accessor :arguments 
     attr_accessor :source
 
-    def initialize(name, arguments, source)
+    def initialize(name, arguments, source, proc = nil)
       raise ArgumentError if name.nil? || name.empty? || !name.is_a?(String)
       raise ArgumentError if source.nil? || source.empty? || !source.is_a?(String)
       raise ArgumentError unless arguments.all? { |arg| VALID_ARGUMENTS.include? arg }
@@ -16,18 +16,7 @@ module Holidays
       @name = name
       @arguments = arguments
       @source = source
-      @result_cache = {}
-    end
-
-    def initialize(name, arguments, source, proc)
-      raise ArgumentError if name.nil? || name.empty? || !name.is_a?(String)
-      raise ArgumentError if source.nil? || source.empty? || !source.is_a?(String)
-      raise ArgumentError unless arguments.all? { |arg| VALID_ARGUMENTS.include? arg }
-
-      @name = name
-      @arguments = arguments
-      @source = source
-      @proc = proc
+      @proc = proc if proc
       @result_cache = {}
     end
 
@@ -51,11 +40,15 @@ module Holidays
     # Calls will be cached to avoid extra computation for the same dates.
     def call(input_args)
       args_list = []
+      # Iterate over all of the arguments that this method takes, and try to get the correct values from
+      # the input list.
       arguments.each do |arg|
         if arg == :date
           args_list << Date.civil(input[:year], input[:month], input[:day])
         elsif input = input_args[arg]
           args_list << input
+        else
+          raise ArgumentError, "Missing required argument #{arg} for custom method #{name}"
         end
       end
 
