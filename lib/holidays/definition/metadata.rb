e5efca837f29880b77ebb2b83754d903fde01b75
mjoerussell <mjoerussell@gmail.com>
2023-08-08 13:20:41 -0500

    Make some updates to region metadata

--- lib/holidays/definition/metadata.rb
+++ lib/holidays/definition/metadata.rb
@@ -1,32 +1,48 @@
 module Holidays
   class Metadata
-    attr_reader :name
-    attr_reader :region
-    
-    def initialize(name, region)
-      @name = name
-      @region = region
+    def initialize(name, region, description)
+      @data = { 
+        name: name, 
+        region: region,
+        description: description,
+      }
     end
 
+    # Load metadata from the definition YAML file. `name` and `region` are required parameters. This also checks for a
+    # `description` which can be used to give a little extra context to a calendar, but it can be nil.
     def self.from_yaml(metadata_def)
-      raise ArgumentError, "Expected region to be defined in metadata block" unless metadata_def["region"]
+      raise ArgumentError, "Expected name to be defined in metadata block" unless metadata_def['name']
+      raise ArgumentError, "Expected region to be defined in metadata block" unless metadata_def['region']
 
       Metadata.new(
         metadata_def['name'],
-        metadata_def['region'].to_sym
+        metadata_def['region'].to_sym,
+        metadata_def['description'],
       )
     end
 
     def [](key)
-      if key == :name
-        name
-      elsif key == :region
-        region
-      end
+      @data[key]
+    end
+
+    def []=(key, value)
+      @data[key] = value
+    end
+
+    def name
+      @data[:name]
+    end
+
+    def region
+      @data[:region]
+    end
+
+    def description
+      @data[:description]
     end
 
     def to_h
-      { name: name, region: region }
+      @data
     end
   end
 end
